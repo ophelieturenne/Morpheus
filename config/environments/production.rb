05225046e63d3304657307e81bf58a123c5cce19
ophelie turenne <turenneophelie@gmail.com>
2023-05-06 19:14:35 +0400

    config host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,10 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  ## config host
+  config.hosts << "aknologia.maurice.webcup.hodi.host"
+
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
