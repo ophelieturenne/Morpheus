685a4299e5d1679789d4e97231738327f4299a7c
guillaume juste <guillaume.juste0408@gmail.com>
2023-05-07 09:41:55 +0400

    add params

--- app/controllers/predictions_controller.rb
+++ app/controllers/predictions_controller.rb
@@ -9,8 +9,8 @@ class PredictionsController < ApplicationController
     if verify_recaptcha
       @role = "Ton rôle : tu es une IA qui fait des prédictions basées sur les rêves fournis par les utilisateurs. Tu dois analyser leurs rêves, les décoder et fournir une prédiction prémonitoire personnalisée ainsi que des informations utiles sur les événements à venir. Tes prédictions peuvent être vraies ou fausses, cela n'a pas d'importance. Tu dois simplement fournir une prédiction. Et n'oublie pas de toujours commencer ta prédiction par 'Je prédis que...'. Je ne veux pas voir de phrases comme 'Je pense que...' ou 'Il serait judicieux de voir un professionnel'. Tu dois simplement fournir une prédiction sans conseiller l'utilisateur de voir un professionnel. nombre maximum de mots : 60"
       @reve = params[:message]
-      @prediction_result = ChatgptService.call("Mon reve : #{params[:message]}. #{@role}")
-      @prediction = current_user.predictions.create(dream: @reve, prediction: @prediction_result)
+      @prediction_result = ChatgptService.call("Mon reve : #{prediction_params[:message]}. #{@role}")
+      @prediction = current_user.predictions.create(dream: prediction_params[:message], prediction: @prediction_result)
       if @prediction.save
         redirect_to prediction_path(@prediction)
       else
@@ -32,6 +32,12 @@ class PredictionsController < ApplicationController
     @prediction = Prediction.find(params[:id])
     @reve = @prediction.dream
     @prediction_result = @prediction.prediction
+  end
+
+  private
+
+  def prediction_params
+    params.permit(:message)
   end
   
 end
