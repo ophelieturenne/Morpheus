46b8e63cbb26eed19a9994590110b290b7b69813
guillaume juste <guillaume.juste0408@gmail.com>
2023-05-07 06:17:00 +0400

    resolve home conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "about", to: "pages#about"
   resources :predictions, only: [:create, :show, :index]
 end
